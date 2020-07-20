43518c4c00e3f5fd0dd300e0ef7eec3d7d2bb2a6
specialprocedures <goodrichian@gmail.com>
2020-07-20 21:42:40 +0400

    update for oc layout

--- domestic-violence/charts.py
+++ domestic-violence/charts.py
@@ -8,8 +8,8 @@ colors = ["#d7191c", "#fdae61", "#ffffbf", "#abd9e9", "#2c7bb6",][::-1]
 
 #%%
 
-ch1_title_ka = "თქვენი აზრით, რა შეიძლება იყოს ოჯახური ძალადობის შეუტყობინებლობის<br>[თანამშრომელი ყოყმანებს] მიზეზი?"
-ch1_title_en = "What do you think would be a reason [for a colleague being hesitant]<br>to report domestic violence?"
+ch1_title_ka = "თქვენი აზრით, რა შეიძლება იყოს ოჯახური ძალადობის<br>შეუტყობინებლობის [თანამშრომელი ყოყმანებს] მიზეზი?"
+ch1_title_en = "What do you think would be a reason [for a colleague being<br>hesitant] to report domestic violence?"
 
 
 def stacked_bar(lang, title, suffix, n):
@@ -24,16 +24,17 @@ def stacked_bar(lang, title, suffix, n):
         barmode="stack",
         orientation="h",
         text="value",
-        width=800,
-        height=600,
+        width=800 * 0.87,
+        height=600 * 0.87,
         labels={"x": None, "y": None},
         title=title,
         color_discrete_sequence=colors,
     )
     fig.update_traces(
-        texttemplate="%{text:.0f}", textfont_size=11, textposition="inside"
+        texttemplate="%{text:.0f}", textfont_size=8, textposition="inside"
     )
-    fig.update_layout(xaxis=dict(range=[0, 100]),)
+    fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
+    fig.update_layout(xaxis=dict(range=[0, 100], showticklabels=False),)
     fig.update_layout(
         legend=dict(
             orientation="h",
@@ -49,7 +50,7 @@ def stacked_bar(lang, title, suffix, n):
     )
     fig.update_yaxes(automargin=True, title=None, tickfont={"size": 10})
     fig.update_xaxes(automargin=True, title=None)
-
+    fig.show()
     fig.write_html(f"{lang}/chart_{suffix}_{n}.html")
 
 
@@ -58,29 +59,39 @@ stacked_bar("en", ch1_title_en, None, "1")
 stacked_bar("ka", None, "no_title", "1")
 stacked_bar("en", None, "no_title", "1")
 # %%
-df = pd.read_csv('ka/chart2.csv')
 
-df.head()
+ch2_title_ka = 'შედეგები ექსპერიმენტული ჯგუფების მიხედვით (%)'
+ch2_title_en = 'Outcome by treatment group (%)'
 
-fig = go.Figure()
-# title = 'შედეგები ექსპერიმენტული ჯგუფების მიხედვით (%)'
+def group_bar(lang, title, suffix):
+    df = pd.read_csv(f'{lang}/chart2.csv')
+    fig = go.Figure()
+    x = df[["label", "group"]].T
+    for n, y_lab in enumerate(df.columns[2:]):
+        fig.add_bar(x=x, y=df[y_lab], name=y_lab, text=df[y_lab], textfont=dict(size=8,))
+        fig["data"][n]["marker"].update({"color": colors[n]})
+    fig.update_layout(
+        title=None,
+        width=800 * 0.87,
+        height=600 * 0.87,
+        legend_orientation="h",
+        xaxis_title="",
+        yaxis_title="",
+        legend=dict(x=-0, y=-0.7, yanchor='bottom'),
 
-x = df[["label", "group"]].T
-for n, y_lab in enumerate(df.columns[2:]):
-    fig.add_bar(x=x, y=df[y_lab], name=y_lab, text=df[y_lab], textfont=dict(size=8,))
-    fig["data"][n]["marker"].update({"color": colors[n]})
-fig.update_layout(
-    title=None,
-    width=800,
-    height=600,
-    legend_orientation="h",
-    xaxis_title="",
-    yaxis_title="",
-    legend=dict(x=-0, y=-0.5, yanchor='bottom'),
+    )
+    fig["layout"]["yaxis1"].update(range=[0, 100])
+    fig.update_traces(texttemplate="%{text:.0f}", textposition="outside")
+    
+    fig.show()
+    fig.write_html(f"{lang}/chart{suffix}2.html")
 
-)
-fig["layout"]["yaxis1"].update(range=[0, 100])
-fig.update_traces(texttemplate="%{text:.0f}", textposition="outside")
 
-fig.show()
-fig.write_html("ka/chart_no_title_2.html")
+group_bar("ka", ch2_title_ka, None)
+group_bar("en", ch2_title_en, None)
+group_bar("ka", None, "_no_title_")
+group_bar("en", None, "_no_title_")
+
+
+
+# %%
